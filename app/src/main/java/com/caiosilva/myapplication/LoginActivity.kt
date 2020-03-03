fd15c13923a534019bfb21d028bae3055c85103b
Caio Silva <cl.silva89@live.com>
2020-03-03 18:14:40 -0300

    link da criação de cadastro no login

--- app/src/main/java/com/caiosilva/myapplication/LoginActivity.kt
+++ app/src/main/java/com/caiosilva/myapplication/LoginActivity.kt
@@ -1,12 +1,20 @@
 package com.caiosilva.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val createAccount: TextView = findViewById(R.id.login_create_account_tv)
+
+        createAccount.setOnClickListener {
+            startActivity(Intent(this, CreateAccountActivity::class.java))
+        }
     }
 }
