7e55f5602b4811350aa09f604ea56c8be619ccec
Caio Silva <cl.silva89@live.com>
2020-03-10 16:42:22 -0300

    camera

--- app/src/main/java/com/caiosilva/myapplication/activity/SettingsActivity.kt
+++ app/src/main/java/com/caiosilva/myapplication/activity/SettingsActivity.kt
@@ -1,10 +1,13 @@
 package com.caiosilva.myapplication.activity
 
 import android.content.DialogInterface
+import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.provider.MediaStore
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.Toolbar
 import com.caiosilva.myapplication.R
@@ -19,6 +22,9 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        val openGallery = findViewById<ImageButton>(R.id.open_gallery_setting_iv)
+        val openCameraBtn = findViewById<ImageButton>(R.id.open_camera_setting_iv)
+
         Permissions().validatePermission(permissions, this, 1)
 
         val toolbar = findViewById<Toolbar>(R.id.main_toolbar)
@@ -26,6 +32,15 @@ class SettingsActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        openCameraBtn.setOnClickListener {
+            val camera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if(camera.resolveActivity(packageManager) != null) {
+                startActivityForResult(camera, REQUEST_CAMERA)
+            } else {
+                Toast.makeText(this, R.string.camera_error_access, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     override fun onRequestPermissionsResult(
@@ -52,5 +67,10 @@ class SettingsActivity : AppCompatActivity() {
 
         alertDialog.create().setCancelable(false)
         alertDialog.show()
+    }
+
+    companion object {
+        const val REQUEST_CAMERA = 100
+        const val REQUEST_GALLERY = 200
     }
 }
